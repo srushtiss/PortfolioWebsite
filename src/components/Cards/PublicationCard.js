c39543285866a6273d2822cf7d99e4eedd31fdc6
srushtiss <sachdevsrushti@gmail.com>
2023-06-30 21:18:16 -0500

    Contact page intiated

--- src/components/Cards/PublicationCard.js
+++ src/components/Cards/PublicationCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Card = (props) => {
+const PublicationCard = (props) => {
   return (
       <div className="m-4 p-4 box-border h-auto w-80 border-4">
           <h1 className="p-4 text-2xl"><h1 className='font-bold'>Title : </h1>{props.title}</h1>
@@ -13,4 +13,4 @@ const Card = (props) => {
   )
 }
 
-export default Card
+export default PublicationCard
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='m-6 flex flex-wrap justify-between items-center'>
+        <Link to="https://www.linkedin.com/in/srushtisachdev/">
+          <div className='p-6 box-border border-4 rounded-3xl bg-blue-300'>
+          <img className='m-auto w-16' src={require("../../utils/linkedin.png")} alt="LinkedIn"/>
+          <h3>Username : srushtisachdev</h3>
+          </div>
+        </Link>
+        <Link to="https://github.com/srushtiss">
+          <div className='m-4 p-4 box-border border-4 rounded-3xl bg-slate-400'>
+          <img className='m-auto w-16' src={require("../../utils/github.png")} alt="Github"/>
+          <h3>Username : srushtiss</h3>
+          </div>
+        </Link>
+        <Link to="https://mail.google.com/mail/?view=cm&fs=1&to=sachdevsrushti@gmail.com&su=SUBJECT&body=BODY">
+          <div className='m-4 p-4 box-border border-4 rounded-3xl bg-red-300'>
+          <img className='m-auto w-16' src={require("../../utils/gmail.png")} alt="Gmail"/>
+          <h3>Username : sachdevsrushti@gmail.com</h3>
+          </div>
+        </Link>
+      </div>
+    </div>
   )
 }
 
