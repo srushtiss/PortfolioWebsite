e6c1a390c6858331399d0051716644022732426e
srushtiss <sachdevsrushti@gmail.com>
2023-07-04 18:14:38 -0500

    Text correction

--- src/components/About Me/AboutMe.js
+++ src/components/About Me/AboutMe.js
@@ -12,16 +12,19 @@ const AboutMe = () => {
             </div>
             <div className="m-8 p-8 text-justify pt-2">
               <p className='m-auto indent-8 text-3xl leading-normal font-about'>
-              The above quote is the reason why I believe I am good at my profession.
-              As a software engineer, everyday learning has become a part of my life.
-              Weather learning technical skills such as programming languages, new software tools, algorithms or soft skills, nor a single day goes without learning.
-              Subsequently, I have developed passion not only for computers but also for learning.
-              Thus, I believe that learning helps me to grow.
+              The above quote is the reason why I believe I am good at my profession. 
+              As a software engineer, everyday learning has become a part of my life. 
+              Whether learning technical skills such as programming languages, new software tools, algorithms, or soft skills, not a single day goes without learning.
+              Subsequently, I have developed a passion not only for computers but also for learning.
+              Thus, I believe that learning helps me to grow. 
               Learning skills and then applying those to make innovative projects will always remain a real joy for me.
               </p>
-              <p className='m-auto indent-8 text-3xl leading-normal font-about'>Apart from being on my laptop, I am also an avid book reader. I have read tons of books from my childhood.</p>
               <p className='m-auto indent-8 text-3xl leading-normal font-about'>
-              When I am not in mood of bieng a techie or a bookworm, I like to play Table Tennis which helps me to rejuvenate my energy.
+              Apart from being on my laptop, I am also an avid book reader. 
+              I have read tons of books from my childhood.
+              </p>
+              <p className='m-auto indent-8 text-3xl leading-normal font-about'>
+              When I am not in the mood of being a techie or a bookworm, I like to play Table Tennis which helps me to rejuvenate my energy.
               </p>
             </div>
            
