8255810954b72bfd17afa9141708de41a8c101a7
srushtiss <sachdevsrushti@gmail.com>
2023-07-04 16:50:13 -0500

    Download resume feature added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,23 @@ import React from 'react'
 import {Link} from "react-router-dom"
 
 const Header = () => {
+
+  const downloadResume=()=>{
+    fetch('Sachdev_Srushti.pdf').then(response => {
+      response.blob().then(blob => {
+          const fileURL = window.URL.createObjectURL(blob);
+          let alink = document.createElement('a');
+          alink.href = fileURL;
+          alink.download = 'Sachdev_Srushti.pdf';
+          alink.click();
+      })
+  })
+  }
+
   return (
-    <div className='flex justify-between border border-solid bg-slate-100 font-navbar text-2xl'>
+    <div className='flex justify-between items-center border border-4 bg-slate-100 font-navbar text-2xl'>
       <h1 className='p-2 m-4'>Srushti</h1>
+      <button onClick={downloadResume} className='p-2 m-4 w-[200px] h-[70px] rounded-3xl bg-sky-500 hover:bg-sky-700 w-40'>Download My Resume</button>
       <ul className='flex'>
         <li className='p-2 m-4'><Link to="/">Home</Link></li>
         <li className='p-2 m-4'><Link to="/about">About Me</Link></li>
